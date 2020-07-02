444742cd80da05d0e194cb6f45ccfd32fe58c9a4
SaulAnto19 <saulantonio0317@gmail.com>
2020-07-01 18:40:26 -0600

    completo Adivinanza RGB

--- AdivinanzaRGB/js/rgb.js
+++ AdivinanzaRGB/js/rgb.js
@@ -1,9 +1,58 @@
-var colores = generarColoresAleatorios(6);
+var numCuadros = 6;
+var colores = generarColoresAleatorios(numCuadros);
 
 var cuadros = document.querySelectorAll(".cuadro");
 var colorsele = seleccionarColor();
 var colorMostrado = document.getElementById("colorMostrado");
 var mensaje = document.querySelector("#mensaje");
+var h1 = document.querySelector("h1");
+var btnReset = document.getElementById("reset");
+var btnFacil = document.getElementById("btnFacil");
+var btnDificil = document.getElementById("btnDificil");
+
+btnFacil.addEventListener("click", function() {
+    btnDificil.classList.remove("seleccionado");
+    btnFacil.classList.add("seleccionado");
+    numCuadros = 3;
+    colores = generarColoresAleatorios(numCuadros);
+    colorsele = seleccionarColor();
+    colorMostrado.textContent = colorsele;
+    for (var i = 0; i < cuadros.length; i++) {
+        if (colores[i]) {
+            cuadros[i].style.backgroundColor = colores[i];
+        } else {
+            cuadros[i].style.display = "none";
+        }
+    }
+});
+
+btnDificil.addEventListener("click", function() {
+    btnFacil.classList.remove("seleccionado");
+    btnDificil.classList.add("seleccionado");
+    numCuadros = 6;
+    colores = generarColoresAleatorios(numCuadros);
+    colorsele = seleccionarColor();
+    colorMostrado.textContent = colorsele;
+    for (var i = 0; i < cuadros.length; i++) {
+        cuadros[i].style.backgroundColor = colores[i];
+        cuadros[i].style.display = "block";
+    }
+});
+
+btnReset.addEventListener("click", function() {
+    //generar colores nuevos
+    colores = generarColoresAleatorios(6);
+    //selecciona un color aleatorio del arreglo
+    colorsele = seleccionarColor();
+    //cambiar el color mostrado por el color seleccionado
+    colorMostrado.textContent = colorsele;
+    mensaje.textContent = "Colores Nuevos";
+    //cambiar los colores del cuadro
+    for (var i = 0; i < cuadros.length; i++) {
+        cuadros[i].style.backgroundColor = colores[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+});
 
 colorMostrado.textContent = colorsele;
 
@@ -17,7 +66,9 @@ for (var i = 0; i < cuadros.length; i++) {
         //comparar color con color seleccionado
         if (colorClickeado === colorsele) {
             mensaje.textContent = "Correcto!";
+            btnReset.textContent = "Volver a jugar?";
             cambiarcolores(colorClickeado);
+            h1.style.backgroundColor = colorClickeado;
         } else {
             this.style.backgroundColor = "#232323";
             mensaje.textContent = "Intente de nuevo!";
@@ -56,9 +107,9 @@ function colorAleatorio() {
     //seleccionar R (rojo) 0 - 255
     var r = Math.floor(Math.random() * 256);
     //seleccionar G (verde) 0 - 255
-    var g = Math.floor(Math.random * 256);
+    var g = Math.floor(Math.random() * 256);
     //seleccionar B (Azul) 0 - 255
-    var b = Math.floor(Math.random * 256);
+    var b = Math.floor(Math.random() * 256);
     //generar la cadena "RGB(r, g, b)"
     return "rgb(" + r + ", " + g + ", " + b + ")";
 };
